b271ab5a9a3c8a9e1abecb71c1b69e149bbfb0e0
asadbabayev <asadyb@code.edu.az>
2024-09-12 17:30:58 +0400

    remove states & add react-hook-form

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import { postProduct } from "../services/postProduct.js";
 import { uploadImageToCloud } from "../services/uploadImage.js";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("");
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  const [imageArray, setImageArray] = useState([]);
-  const [imageUrl, setImageUrl] = useState(null);
-  const [imagePreview, setImagePreview] = useState("");
-  const [newArrival, setNewArrival] = useState(false);
-  const [topSelling, setTopSelling] = useState(false);
+  const [category, setCategory] = useState([]);
+  const [coverImage, setCoverImage] = useState(null);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async(values) => {
+
+    const response = await postProduct(newProduct);
+    const data = await response.json();
+    console.log(values);
+    console.log(data);
+  };
 
   const handleAddProduct = async (e) => {
     e.preventDefault();
-    let uploadedImageUrl = "";
-
-    if (imageUrl) {
-      uploadedImageUrl = await uploadImageToCloud(imageUrl);
-    }
-
     const newProduct = {
       title,
       description,
-      coverImage: uploadedImageUrl,
+      coverImage,
       price,
       // size,
       // color,
@@ -36,59 +36,47 @@ const AddProduct = () => {
       // topSeller,
     };
 
-    try {
-      const response = await postProduct(newProduct);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Error adding product:", error);
-    }
+    const response = await postProduct(newProduct);
+    const data = await response.json();
+    console.log(data)
   };
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setImageUrl(file);
-    setImageArray([...imageArray, URL.createObjectURL(file)]);
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePreview(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
+  const handleImage = async (e) => {
+    const URL = await uploadImageToCloud(e.target.files[0]);
+    setCoverImage(URL);
   };
-
   return (
     <div className="container mx-auto">
       <h2 className="text-2xl font-semibold mb-6">Add Product</h2>
       <form
-        onSubmit={handleAddProduct}
+        //
+        onSubmit={handleSubmit(onSubmit)}
+        //
+        // onSubmit={handleAddProduct}
         className="bg-white p-6 rounded-lg grid grid-cols-2 gap-8"
       >
         <div>
           <label className="block mb-2 font-medium">Title</label>
           <input
+            {...register("title")}
             type="text"
             name="title"
             className="w-full border p-2 rounded mb-4"
             required
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
           />
 
           <label className="block mb-2 font-medium">Price</label>
           <input
+            {...register("price")}
             type="number"
             name="price"
             className="w-full border p-2 rounded mb-4"
             required
-            onChange={(e) => setPrice(e.target.value)}
-            value={price}
           />
 
           <label className="block mb-2 font-medium">Category</label>
           <input
+            {...register("category")}
             type="text"
             name="category"
             className="w-full border p-2 rounded mb-4"
@@ -98,22 +86,20 @@ const AddProduct = () => {
 
           <label className="block mb-2 font-medium">Description</label>
           <textarea
+            {...register("description")}
             name="description"
             className="w-full border p-2 rounded mb-4 h-28"
             required
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
           />
         </div>
         <div>
           <label className="block mb-2 font-medium">Quantity</label>
           <input
+            {...register("quantity")}
             type="number"
             name="quantity"
             className="w-full border p-2 rounded mb-4"
             required
-            onChange={(e) => setQuantity(e.target.value)}
-            value={quantity}
           />
 
           <label className="block mb-2 font-medium">Images</label>
@@ -121,13 +107,13 @@ const AddProduct = () => {
             <input
               type="file"
               name="images"
-              onChange={handleImageChange}
+              onChange={handleImage}
               multiple
               className="block"
             />
-            {imageUrl && (
+            {coverImage && (
               <div className="w-20 h-20 mt-4">
-                <img src={imagePreview} alt="Preview" />
+                <img src={coverImage} alt="Preview" />
               </div>
             )}
           </div>
@@ -154,19 +140,17 @@ const AddProduct = () => {
 
           <label className="block mb-2 font-medium">New Arrival</label>
           <input
+            {...register("newArrival")}
             type="checkbox"
             name="newArrival"
-            onChange={(e) => setNewArrival(e.target.checked)}
-            checked={newArrival}
             className="mr-2"
           />
 
           <label className="block mb-2 font-medium">Top Seller</label>
           <input
+            {...register("topSeller")}
             type="checkbox"
             name="topSeller"
-            onChange={(e) => setTopSelling(e.target.checked)}
-            checked={topSelling}
             className="mr-2"
           />
         </div>
